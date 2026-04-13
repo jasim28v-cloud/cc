60217c1cd4b459ac305e0d7f70c569073d500608
jasim28v-cloud <jasim28v@gmail.com>
2026-04-13 12:37:32 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,4 @@
 // js/firebase-config.js
-// إعدادات Firebase الكاملة لمشروع "قالبك"
-
-// استيراد الوظائف المطلوبة (إذا كنتِ تستخدمين npm)
-// import { initializeApp } from "firebase/app";
-// import { getDatabase } from "firebase/database";
-
-// إعدادات Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCT0GTVZSv3d48qP3_2auOtibkjD00cUMA",
   authDomain: "gomrka-420d0.firebaseapp.com",
@@ -13,12 +6,9 @@ const firebaseConfig = {
   projectId: "gomrka-420d0",
   storageBucket: "gomrka-420d0.firebasestorage.app",
   messagingSenderId: "581820766419",
-  appId: "1:581820766419:web:b8f05224532782be5a5c26",
-  measurementId: "G-61DFP9M8BQ"
+  appId: "1:581820766419:web:b8f05224532782be5a5c26"
 };
 
-// تهيئة Firebase (للاستخدام المباشر في المتصفح)
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-
-console.log("✅ Firebase Connected: gomrka-420d0");
+const storage = firebase.storage();
